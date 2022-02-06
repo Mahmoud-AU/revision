8ee9fb0cc65911d0aff1130eae859b3fe7391d57
Mahmoud-AU <98646046+Mahmoud-AU@users.noreply.github.com>
2022-02-06 19:15:41 +1100

    Add files via upload
    Test commit

---
+++ function.js
@@ -0,0 +1,339 @@
+//#region SURAH LIST
+let surah1 = new Surah("Al-Fatihah", 1, [1], null, 1);
+let surah2_1 = new Surah("Al-Baqarah", 21, [1], "Juz 1", 2);
+let surah2_2 = new Surah("Al-Baqarah", 20, [2], "Juz 2", 3);
+let surah2_3 = new Surah("Al-Baqarah", 8, [3], "Juz 3", 4);
+let surah3 = new Surah("Ali Imran", 27, [3, 4], null, 5);
+let surah4 = new Surah("An-Nisa'", 30, [4, 5, 6], null, 6);
+let surah5 = new Surah("Al-Ma'idah", 23, [6, 7], null, 7);
+let surah6 = new Surah("Al-An'am", 23, [7, 8], null, 8);
+let surah7 = new Surah("Al-A'raf", 26, [8, 9], null, 9);
+let surah8 = new Surah("Al-Anfal", 10, [9, 10], null, 10);
+let surah9 = new Surah("At-Tawbah", 21, [10, 11], null, 11);
+let surah10 = new Surah("Yunus", 14, [11], null, 12);
+let surah11 = new Surah("Hud", 15, [11, 12], null, 13);
+let surah12 = new Surah("Yusuf", 14, [12, 13], null, 14);
+let surah13 = new Surah("Ar-Ra'd", 6, [13], null, 15);
+let surah14 = new Surah("Ibraheem", 7, [13], null, 16);
+let surah15 = new Surah("Al-Hijr", 6, [14], null, 17);
+let surah16 = new Surah("An-Nahl", 15, [14], null, 18);
+let surah17 = new Surah("Al-Isra'", 12, [15], null, 19);
+let surah18 = new Surah("Al-Kahf", 12, [15, 16], null, 20);
+let surah19 = new Surah("Maryam", 8, [16], null, 21);
+let surah20 = new Surah("Ta-Ha", 10, [16], null, 22);
+let surah21 = new Surah("Al-Anbiya'", 10, [17], null, 23);
+let surah22 = new Surah("Al-Hajj", 10, [17], null, 24);
+let surah23 = new Surah("Al-Mu'minoon", 8, [18], null, 25);
+let surah24 = new Surah("An-Noor", 10, [18], null, 26);
+let surah25 = new Surah("Al-Furqan", 8, [18, 19], null, 27);
+let surah26 = new Surah("Ash-Shu'ara'", 10, [19], null, 28);
+let surah27 = new Surah("An-Naml", 9, [19, 20], null, 29);
+let surah28 = new Surah("Al-Qasas", 12, [20], null, 30);
+let surah29 = new Surah("Al-Ankabut", 9, [20, 21], null, 31);
+let surah30 = new Surah("Ar-Rum", 8, [21], null, 32);
+let surah31 = new Surah("Luqman", 4, [21], null, 33);
+let surah32 = new Surah("As-Sajdah", 3, [21], null, 34);
+let surah33 = new Surah("Al-Ahzab", 10, [21, 22], null, 35);
+let surah34 = new Surah("As-Saba'", 7, [22], null, 36);
+let surah35 = new Surah("Fatir", 6, [22], null, 37);
+let surah36 = new Surah("Ya-Seen", 6, [22, 23], null, 38);
+let surah37 = new Surah("As-Saffat", 7, [23], null, 39);
+let surah38 = new Surah("Saad", 5, [23], null, 40);
+let surah39 = new Surah("Az-Zumar", 9, [23, 24], null, 41);
+let surah40 = new Surah("Ghafir", 10, [24], null, 42);
+let surah41 = new Surah("Fussilat", 6, [24, 25], null, 43);
+let surah42 = new Surah("Ash-Shura", 7, [25], null, 44);
+let surah43 = new Surah("Az-Zukhruf", 7, [25], null, 45);
+let surah44 = new Surah("Ad-Dukhan", 3, [25], null, 46);
+let surah45 = new Surah("Al-Jathiyah", 4, [25, 26], null, 47);
+let surah46 = new Surah("Al-Ahqaf", 5, [26], null, 48);
+let surah47 = new Surah("Muhammad", 4, [26], null, 49);
+let surah48 = new Surah("Al-Fath", 5, [26], null, 50);
+let surah49 = new Surah("Al-Hujurat", 3, [26], null, 51);
+let surah50 = new Surah("Qaaf", 3, [26], null, 52);
+let surah51 = new Surah("Adh-Dhariyaat", 3, [26, 27], null, 53);
+let surah52 = new Surah("At-Tur", 3, [27], null, 54);
+let surah53 = new Surah("An-Najm", 3, [27], null, 55);
+let surah54 = new Surah("Al-Qamar", 3, [27], null, 56);
+let surah55 = new Surah("Ar-Rahman", 3, [27], null, 57);
+let surah56 = new Surah("Al-Wa'qiah", 3, [27], null, 58);
+let surah57 = new Surah("Al-Hadeed", 4, [27], null, 59);
+let surah58 = new Surah("Al-Mujadilah", 3, [28], null, 60);
+let surah59 = new Surah("Al-Hashr", 4, [28], null, 61);
+let surah60 = new Surah("Al-Mumtahanah", 3, [28], null, 62);
+let surah61 = new Surah("As-Saff", 2, [28], null, 63);
+let surah62 = new Surah("Al-Jumu'ah", 2, [28], null, 64);
+let surah63 = new Surah("Al-Munafiqoon", 2, [28], null, 65);
+let surah64 = new Surah("At-Taghabun", 2, [28], null, 66);
+let surah65 = new Surah("At-Talaq", 2, [28], null, 67);
+let surah66 = new Surah("At-Tahreem", 2, [28], null, 68);
+let surah67 = new Surah("Al-Mulk", 3, [29], null, 69);
+let surah68 = new Surah("Al-Qalam", 3, [29], null, 70);
+let surah69 = new Surah("Al-Haqqah", 3, [29], null, 71);
+let surah70 = new Surah("Al-Ma'arij", 3, [29], null, 72);
+let surah71 = new Surah("Nuh", 2, [29], null, 73);
+let surah72 = new Surah("Al-Jinn", 2, [29], null, 74);
+let surah73 = new Surah("Al-Muzzammil", 2, [29], null, 75);
+let surah74 = new Surah("Al-Muddaththir", 3, [29], null, 76);
+let surah75 = new Surah("Al-Qiyamah", 2, [29], null, 77);
+let surah76 = new Surah("Al-Insaan", 3, [29], null, 78);
+let surah77 = new Surah("Al-Mursalaat", 2, [29], null, 79);
+let surah78 = new Surah("An-Naba'", 2, [30], null, 80);
+let surah79 = new Surah("An-Nazi'at", 2, [30], null, 81);
+let surah80 = new Surah("'Abasa", 1, [30], null, 82);
+let surah81 = new Surah("At-Takweer", 1, [30], null, 83);
+let surah82 = new Surah("Al-Infitar", 1, [30], null, 84);
+let surah83 = new Surah("Al-Mutaffifeen", 2, [30], null, 85);
+let surah84 = new Surah("Al-Inshiqaq", 1, [30], null, 86);
+let surah85 = new Surah("At-Tariq", 1, [30], null, 1, 87);
+let surah86 = new Surah("Al-Burooj", 1, [30], null, 88);
+let surah87 = new Surah("Al-A'la", 1, [30], null, 89);
+let surah88 = new Surah("Al-Ghashiyah", 1, [30], null, 90);
+let surah89 = new Surah("Al-Fajr", 2, [30], null, 91);
+let surah90 = new Surah("Al-Balad", 1, [30], null, 92);
+let surah91 = new Surah("Ash-Shams", 1, [30], null, 93);
+let surah92 = new Surah("Al-Layl", 1, [30], null, 94);
+let surah93 = new Surah("Ad-Duha", 1, [30], null, 95);
+let surah94 = new Surah("Ash-Sharh", 1, [30], null, 96);
+let surah95 = new Surah("At-Tin", 1, [30], null, 97);
+let surah96 = new Surah("Al-'Alaq'", 1, [30], null, 98);
+let surah97 = new Surah("Al-Qadr", 1, [30], null, 99);
+let surah98 = new Surah("Al-Bayyinah", 1, [30], null, 100);
+let surah99 = new Surah("Al-Zalzalah", 1, [30], null, 101);
+let surah100 = new Surah("Al'Adiyaat", 1, [30], null, 102);
+let surah101 = new Surah("Al-Qari'ah", 1, [30], null, 103);
+let surah102 = new Surah("At-Takathur", 1, [30], null, 104);
+let surah103 = new Surah("Al-'Asr", 1, [30], null, 105);
+let surah104 = new Surah("Al-Humazah", 1, [30], null, 106);
+let surah105 = new Surah("Al-Fil", 1, [30], null, 107);
+let surah106 = new Surah("Quraysh", 1, [30], null, 108);
+let surah107 = new Surah("Al-Ma'oon", 1, [30], null, 109);
+let surah108 = new Surah("Al-Kawthar", 1, [30], null, 110);
+let surah109 = new Surah("Al-Kafiroon", 1, [30], null, 111);
+let surah110 = new Surah("An-Nasr", 1, [30], null, 112);
+let surah111 = new Surah("Al-Masad", 1, [30], null, 113);
+let surah112 = new Surah("Al-Ikhlaas", 1, [30], null, 114);
+let surah113 = new Surah("Al-Falaq", 1, [30], null, 115);
+let surah114 = new Surah("An-Naas", 1, [30], null, 116);
+//#endregion
+
+let surahs = [surah1, surah2_1, surah2_2, surah2_3, surah3, surah4, surah5, surah6, surah7, surah8, surah9, surah10,
+    surah11, surah12, surah13, surah14, surah15, surah16, surah17, surah18, surah19, surah20,
+    surah21, surah22, surah23, surah24, surah25, surah26, surah27, surah28, surah29, surah30,
+    surah31, surah32, surah33, surah34, surah35, surah36, surah37, surah38, surah39, surah40,
+    surah41, surah42, surah43, surah44, surah45, surah46, surah47, surah48, surah49, surah50,
+    surah51, surah52, surah53, surah54, surah55, surah56, surah57, surah58, surah69, surah60,
+    surah61, surah62, surah63, surah64, surah65, surah66, surah67, surah68, surah69, surah70,
+    surah71, surah72, surah73, surah74, surah75, surah76, surah77, surah78, surah79, surah80,
+    surah81, surah82, surah83, surah84, surah85, surah86, surah87, surah88, surah89, surah90,
+    surah91, surah92, surah93, surah94, surah95, surah96, surah97, surah98, surah99, surah100,
+    surah101, surah102, surah103, surah104, surah105, surah106, surah107, surah108, surah109,
+    surah110, surah111, surah112, surah113, surah114];
+
+let memorising = JSON.parse(localStorage.getItem("memorising")) || [];
+let revisionArray = JSON.parse(localStorage.getItem("revisionArray")) || [];
+let practisingArray = JSON.parse(localStorage.getItem("practisingArray")) || [];
+let pagesArray = JSON.parse(localStorage.getItem("pagesArray")) || [];
+
+let allSection = document.getElementById("all");
+let currentSection = allSection;
+
+let isChangingMemorising = false;
+
+let memorisingBox = document.getElementById("memorising");
+let emojisParent = document.querySelector(".selection-area");
+let selectedEmojisParent = document.querySelector(".selected-area");
+
+
+let boxTemplates = document.querySelectorAll('.revisionButton');
+let firstFiveSet = false;
+
+let progress = document.getElementById("progress");
+let goal = document.getElementById("goal");
+let progressBarBG = document.getElementById("progressBarBG");
+let progressBar = document.getElementById("progressBar");
+let goalDropdown = document.querySelector(".goals");
+
+let progressAmount = JSON.parse(localStorage.getItem("progress")) || 0;
+let goalAmount = JSON.parse(localStorage.getItem("goal")) || 10;
+
+let selectionContent = document.getElementById("section-navigation-content");
+
+function Surah(name, points, sectionNumber, juz, id) {
+    this.name = name;
+    this.points = points;
+    this.sectionNumber = sectionNumber;
+    this.juz = juz;
+    this.id = id;
+
+    this.createSelectionButton = function() {
+        let selectionBtn = document.createElement("button");
+        selectionBtn.classList.add("emoji-box");
+
+        var h1Element = document.createElement("h1");
+        var h1Text = document.createTextNode(this.name);
+        h1Element.appendChild(h1Text);
+        selectionBtn.appendChild(h1Element);
+
+        if (this.juz !== null) {
+            var jElement = document.createElement("p");
+            var jText = document.createTextNode(this.juz);
+            jElement.appendChild(jText);
+            selectionBtn.appendChild(jElement);
+        }
+
+        var pElement = document.createElement("p");
+        var pText = document.createTextNode(this.points);
+        pElement.appendChild(pText);
+        selectionBtn.appendChild(pElement);
+
+        emojisParent.appendChild(selectionBtn);
+        selectionBtn.onclick = () => memoriseCheck(selectionBtn, this);
+        selectionBtn.setAttribute("data-emoji", JSON.stringify(this));
+
+        if (memorising.id === this.id) {
+            console.log("MEMORISING");
+        }
+        else {
+            var isPractising = false;
+            for (let i = 0; i < revisionArray.length; i++) {
+                if (revisionArray[i].id === this.id) {
+                    console.log("REVISING");
+                    break;
+                }
+                else isPractising = true;
+            }
+            if (isPractising) {
+                for (let i = 0; i < practisingArray.length; i++) {
+                    if (revisionArray[i].id === this.id) {
+                        console.log("PRACTISING");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    function createInstance() {
+        
+    }
+}
+
+surahs.forEach(element => {
+    element.createSelectionButton();
+});
+
+function memoriseCheck(el, obj) {
+    if (!isChangingMemorising) {
+        setAsRevising(obj);
+    }
+    else {
+        setAsMemorising();
+    }
+    /*if (memorisedArray.length > 0) {
+        for (let mc = 0; mc < memorisedArray.length; mc++) {
+            if (memorisedArray[mc].id !== obj.id && mc === memorisedArray.length - 1) {
+                console.log("ADDED");
+                createInstance(obj);
+                memorisedFunction(el);
+                memorisedArray.push(obj);
+                localStorage.setItem("array", JSON.stringify(memorisedArray));
+                break;
+            }
+            else if (memorisedArray[mc].id === obj.id) {
+                deleteInstance(obj);
+                memorisedArray.splice(mc, 1);
+                el.style.backgroundColor = "white";
+                el.style.boxShadow = "0px 0px 3px white";
+                for (let ffs = 0; ffs < boxTemplates.length; ffs++) {
+                    if (boxTemplates[ffs].children.length === 0) {
+                        firstFiveSet = false;
+                        if (memorisedArray.length > 4) {
+                            updatePositioning(el, obj);
+                        }
+                        break;
+                    }
+                }
+                localStorage.setItem("array", JSON.stringify(memorisedArray));
+                break;
+            }
+        }
+    }
+    else {
+        createInstance(obj);
+        memorisedArray.push(obj);
+        memorisedFunction(el);
+        localStorage.setItem("array", JSON.stringify(memorisedArray));
+    }*/
+}
+
+function createInstance(obj) {
+    var instanceBtn = document.createElement("button");
+    instanceBtn.classList.add("instance-box");
+
+    var h1Element = document.createElement("h1");
+    var h1Text = document.createTextNode(obj.name);
+    h1Element.appendChild(h1Text);
+    instanceBtn.appendChild(h1Element);
+    
+    if (obj.juz !== null) {
+        var jElement = document.createElement("p");
+        var jText = document.createTextNode(obj.juz);
+        jElement.appendChild(jText);
+        instanceBtn.appendChild(jElement);
+    }
+
+    var pElement = document.createElement("p");
+    var pText = document.createTextNode(obj.points);
+    pElement.appendChild(pText);
+    instanceBtn.appendChild(pElement);
+
+    if (!firstFiveSet) firstFive(instanceBtn);
+    else selectedEmojisParent.appendChild(instanceBtn);
+    instanceBtn.setAttribute("data-emoji", JSON.stringify(obj));
+    instanceBtn.onclick = () => reviseCheck(instanceBtn, obj);
+}
+
+function setAsMemorising() {
+    for (let i = 0; i < revisionArray.length; i++) {
+        isChangingMemorising = true;
+    }
+}
+
+function setAsRevising(obj) {
+    createInstance(obj);
+}
+
+function setAsPractising() {
+
+}
+
+function openSurahList() {
+    if (selectionContent.style.display === "inline") selectionContent.style.display = "none";
+    else selectionContent.style.display = "inline";
+}
+
+function setChangeMemorising() {
+    if (!isChangingMemorising) {
+        for (let i = 0; i < emojisParent.children.length; i++) {
+            emojisParent.children[i].classList.add("isTakenClass");
+        }
+        isChangingMemorising = true;
+    }
+    else {
+        for (let i = 0; i < emojisParent.children.length; i++) {
+            emojisParent.children[i].classList.remove("isTakenClass");
+        }
+        isChangingMemorising = false;
+    }
+}
+
+function reset() {
+    localStorage.clear();
+    window.location.reload();
+}
+
+function resetProgress() {
+    localStorage.removeItem("progress");
+    window.location.reload();
+}
